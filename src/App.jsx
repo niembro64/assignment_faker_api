c2a2dd7022cbf33b0fb54071eb36ae9b060bcbb2
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-20 19:24:44 -0400

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-<h1>test eric</h1>
-      </header>
+      <h1>test eric</h1>
     </div>
   );
 }
